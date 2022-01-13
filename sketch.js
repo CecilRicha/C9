e34d06ddf8a427fc3f5d4c15366af4c5b602b4c8
CecilRicha <97675880+CecilRicha@users.noreply.github.com>
2022-01-13 17:56:51 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+
+function setup() {
+  createCanvas(400,600);
+  var ball = createSprite(100,250,90,90);
+ball.velocityX = 5
+
+}
+
+function draw() 
+{
+  background("blue");
+drawSprites();
+}
+
+
+
+
